25e0b9599579be708c215e1cfb6296e4e5893a38
Jitao David Zhang <jitao_david.zhang@roche.com>
2020-02-05 09:23:50 +0100

    assign fread return values to a dummy variable to avoid unsed-reasult warning; not clear yet whether we will use the value

--- src/html.c
+++ src/html.c
@@ -456,13 +456,14 @@ static int cgiGetNextPair (int *first,Stringa item,Stringa value) {
   static char *bufferPtr;
   int mode;
   int c;
+  int fread_res;
 
   if (gCgiBuffer == NULL) {
     char *contentLength = getenv ("CONTENT_LENGTH");
     gCgiBufferSize = contentLength ? atoi (contentLength) : 0;
     if (gCgiBufferSize != 0) {
       gCgiBuffer = (char *)hlr_malloc (gCgiBufferSize);
-      fread (gCgiBuffer,gCgiBufferSize,1,stdin);
+      fread_res = fread (gCgiBuffer,gCgiBufferSize,1,stdin);
     }
     bufferPtr = gCgiBuffer;
   }
