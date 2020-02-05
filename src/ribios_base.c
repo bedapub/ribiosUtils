2323c1bac1bf0831b24b18970973ed08ef5f18f1
Jitao David Zhang <jitao_david.zhang@roche.com>
2020-02-05 07:44:22 +0100

    add lisence information

--- src/ribios_base.c
+++ src/ribios_base.c
@@ -1,3 +1,26 @@
+/* -*- mode: C; c-indent-level: 4; c-basic-offset: 4; indent-tabs-mode: nil; -*- */
+/*
+ *  ribiosBase -- The base package of the ribios software suite
+ *
+ *  Copyright (C) 2019  Jitao David Zhang
+ *
+ *  This file is part of ribiosBase.
+ *
+ *  ribiosBase is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  ribiosBase is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with ribiosBase.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
 #include <R_ext/Rdynload.h>
 #include "R.h"
 #include "ribios_base.h"
