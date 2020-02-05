2134b4a37b318f65a8f32bcf06346f1990220fc7
Jitao David Zhang <jitao_david.zhang@roche.com>
2020-02-05 12:03:58 +0100

    renamed as init.c, and add usual code blocks for source codes in R

--- src/init.c
+++ src/init.c
@@ -21,17 +21,32 @@
  */
 
 
-#include <R_ext/Rdynload.h>
 #include "R.h"
+#include "Rinternals.h"
+#include "R_ext/Rdynload.h"
+
 #include "ribios_base.h"
 
+/* definitions of functions provided for .Call() */
 static const R_CallMethodDef callMethods[] = {
-  /* pwdecode.c */
   {NULL, NULL, 0}
 };
 
-void R_init_ribiosUtils(DllInfo *info) {
+/* definition of functions to be exported */
+void R_init_ribiosBase(DllInfo *info) {
+
+  /* register routines to be called by R code */
   R_registerRoutines(info, NULL, callMethods, NULL, NULL);
-  R_useDynamicSymbols(info, FALSE);
-  R_forceSymbols(info, TRUE);
+  
+  /* the line below says that the DLL is not to be searched
+   * for entry points specified by character strings so
+   * .C etc calls will only find registered symbols
+   */  
+  R_useDynamicSymbols(info, FALSE); 
+
+  /* R_forceSymbols call only allows .C etc calls which 
+   * specify entry points by R objects such as C_routineName
+   * (and not by character strings)
+   */ 
+  R_forceSymbols(info, TRUE); 
 }
