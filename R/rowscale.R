6c5ee76d1503d6f2ef222fb72292659b7271101d
Jitao David Zhang <jitao_david.zhang@roche.com>
2022-01-03 16:40:06 +0100

    add rowscale

--- R/rowscale.R
+++ R/rowscale.R
@@ -37,3 +37,37 @@ rowscale <- function(x, center = TRUE, scale=TRUE) UseMethod("rowscale")
 rowscale.matrix <- function(x, center=TRUE, scale=TRUE) {
   t(scale(t(x),center=center, scale=scale))
 }
+
+#' Scale a table by row
+#' 
+#' Scaling a table by row can be slightly slower due to a transposing step.
+#' 
+#' @param x An matrix
+#' @param center Logical, passed to \code{scale}. 
+#' to \code{TRUE}
+#' @param scale Logical, passed to \code{scale}. 
+#' \code{TRUE}
+#' @return A table with each row scaled.
+#' @author Jitao David Zhang <jitao_david.zhang@@roche.com>
+#' @seealso \code{\link{scale}}
+#' @examples
+#' 
+#' letterDf <- data.frame(from=c("A", "A", "B", "C"), to=c("A", "B", "C", "A"))
+#' tbl <- table(letterDf$from, letterDf$to)
+#' tblRowscale <- rowscale(tbl)
+#' 
+#' print(tbl)
+#' print(tblRowscale)
+#' rowMeans(tblRowscale)
+#' apply(tblRowscale, 1L, sd)
+#' 
+#' rowscale(tbl, center=FALSE, scale=FALSE) ## equal to mat
+#' rowscale(tbl, center=TRUE, scale=FALSE)
+#' rowscale(tbl, center=FALSE, scale=TRUE)
+#' 
+#' @method rowscale table
+#' @export
+rowscale.table <- function(x, center=TRUE, scale=TRUE) {
+  t(scale(t(x),center=center, scale=scale))
+}
+
