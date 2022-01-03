63cbdfbda329a87e5dac906758c65b40a0b6c3b7
Jitao David Zhang <jitao_david.zhang@roche.com>
2022-01-03 15:34:57 +0100

    add equateWellLabelWidth

--- R/microwellPlates.R
+++ R/microwellPlates.R
@@ -18,19 +18,43 @@ wellIndex2position <- function(ind, format=c("96", "384")) {
     nrow <- 16
     ncol <- 24
   }
-  
+
   rowInd <- (ind - 1) %/% ncol + 1
   rowInd[rowInd<1 | rowInd>nrow] <- NA
   rowLetter <- LETTERS[1:nrow][rowInd]
   colInd <- as.integer((ind - 1) %% ncol + 1)
-  
+
   outOfRange <- is.na(ind) | ind < 1 | ind > nrow*ncol
   rowLetter[outOfRange] <- colInd[outOfRange] <- NA
-  
+
   res <- data.frame(WellIndex=ind,
                     Row=rowLetter,
                     Column=colInd,
                     row.names=NULL,
                     stringsAsFactors = FALSE)
+  return(res)
+}
+
+
+#' Format labels for wells in microwell plates to equal widths
+#' @param wells A vector of character strings indicating well positions, they
+#' may be of different widths, for instance A1, A10, A12
+#' @return A vector of the same length, with all labels adjusted to the equal
+#' width, with left-padding zeros added whenever it makes sense. If the input
+#' labels are already of the same length, no change is applied.
+#' @example
+#' equateWellLabelWidth(c("A1", "C10", "D12"))
+#' @export
+equateWellLabelWidth <- function(wells) {
+  stopifnot(is.character(wells))
+  likeWell <- grepl("^[A-Z][0-9]*$", wells)
+  if(any(!likeWell)) {
+    warning("Following wells may have a bad format:", 
+            paste(wells[!likeWell], collapse = ","))
+  }
+  rowLabel <- substr(wells, 1, 1)
+  colLabel <- as.integer(substr(wells, 2, 10))
+  fmt <- sprintf("%%0%dd", floor(log10(max(colLabel, na.rm=TRUE)))+1)
+  res <- paste0(rowLabel, sprintf(fmt, colLabel))
   return(res)
 }
